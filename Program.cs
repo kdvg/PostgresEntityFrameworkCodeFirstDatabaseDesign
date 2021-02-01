556a0918544738649ba5d286b92c2d990b3800ba
kpp 567 <kpp567567@gmail.com>
2021-01-31 16:59:42 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostgresEntityFrameworkCodeFirstDatabaseDesign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // note: 
+            // https://www.compose.com/articles/code-first-database-design-with-entity-framework-and-postgresql/
+            // see this link for origin, as well as notes on upgrading to newer version of EF
+
+
+        }
+    }
+}
